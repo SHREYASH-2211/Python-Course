b3444c4ba4f5ce02893449481d2449a4958f91c9
SHREYASH-2211 <singhshreyash2211@gmail.com>
2024-06-21 21:28:09 +0530

    delete-rename(Project to Python Project)

---
+++ Python-Project/text_to_speech.py
@@ -0,0 +1,13 @@
+from gtts import gTTS
+import os
+
+def text_to_speech(text, lang='en'):
+    tts = gTTS(text=text, lang=lang)
+    tts.save("output.mp3")
+    # os.system("start output.mp3")  # For Windows, use "start"
+    os.system("afplay output.mp3")  # For macOS, use "afplay"
+    # os.system("mpg321 output.mp3")  # For Linux, use "mpg321"
+
+if __name__ == "__main__":
+    text = "Hello mamma khana ban gaya"
+    text_to_speech(text)
